5a2fb68856734f2fb3b1bdef27142629388a52b8
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-08-04 11:59:21 -0500

    [Fix] Added one missing colon

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -6,9 +6,9 @@ file { '/etc/ssh/ssh_config':
 file_line { 'No password authentication':
   path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
-  match => '^PasswordAuthentication'
+  match => '^PasswordAuthentication',
 }->
-file_line { 'Identity file'
+file_line { 'Identity file':
   path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/holberton',
   match => '^IdentityFile'

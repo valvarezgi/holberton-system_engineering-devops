127170fdd7eff68a2e99d087bca3148902fe3a4a
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-09-14 20:14:08 -0500

    [Fix]

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """Dictionary of list of dictionaries"""
-from json import dumps
+import json
 from requests import get
 
 if __name__ == "__main__":
-    data = {}
-    for user_id in get('https://jsonplaceholder.typicode.com/users/').json():
-        users = get('https://jsonplaceholder.typicode.com/users/{}'
-                .format(user_id)).json()
-        todo_list = get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                    .format(user_id)).json()
-        data[str(user_id)] = [dict(task=element['title'], completed=element['completed'],
-                                    username=users['username']) for element in todo_list]
-        
-        with open('todo_all_employees.json', mode='w') as file:
-            file.write(dumps(data))
+
+    user_id = {}
+    filename = "todo_all_employees.json"
+    req = get('https://jsonplaceholder.typicode.com/todos')
+    req_id = get('https://jsonplaceholder.typicode.com/users/')
+    with open("todo_all_employees.json", mode="w") as f:
+        d = {j.get("id"): [{'task': i.get('title'),
+             'completed': i.get('completed'),
+                            'username': j.get('username')} for i in req.json()
+                           if j.get("id") == i.get('userId')]
+             for j in req_id.json()}
+        json.dump(d, f)
